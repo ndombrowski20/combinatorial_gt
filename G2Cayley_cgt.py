9b35ad11898bb9d42de35c78cf017674fa676cf2
nate <ndombrowski20@gmail.com>
2018-10-09 11:26:42 -0400

    G2 (4, 10)
    changed the g2 file to create g2 with 10

--- G2Cayley_cgt.py
+++ G2Cayley_cgt.py
@@ -19,10 +19,10 @@ bB = Word([b, B])
 bbbbbb = Word([b, b, b, b, b, b])
 abAB = Word([a, b, A, B])
 
-g2 = Group([a, b], [aa, aA, bbbbbb, bB, abAB], 12)
+g2 = Group([a, b], [aa, aA, bbbbbb, bB, abAB], 10)
 
 g2_cayley = Cayley()
 
-g2_cayley.read_newgraph(g2, 4, 12)
+g2_cayley.read_newgraph(g2, 4, 10)
 
-g2_cayley.for_saving('dicayley_g2_12', 'info_dicayley_g2_12')
+g2_cayley.for_saving('dicayley_g2_10', 'info_dicayley_g2_10')
